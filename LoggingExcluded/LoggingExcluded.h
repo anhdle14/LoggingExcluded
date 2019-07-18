6dc9cae34dce73f13fde1ee2a90156e14270c30b
Le Anh Duc <anhdle14@hotmail.com>
2019-07-19 01:25:58 +0900

    Initial Commit

---
+++ LoggingExcluded/LoggingExcluded.h
@@ -0,0 +1,19 @@
+//
+//  LoggingExcluded.h
+//  LoggingExcluded
+//
+//  Created by Anh Đức Lê on R 1/07/19.
+//  Copyright © Reiwa 1 Anh Đức Lê. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggingExcluded.
+FOUNDATION_EXPORT double LoggingExcludedVersionNumber;
+
+//! Project version string for LoggingExcluded.
+FOUNDATION_EXPORT const unsigned char LoggingExcludedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingExcluded/PublicHeader.h>
+
+
---
+++ LoggingExcludedTests/LoggingExcludedTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoggingExcludedTests.swift
+//  LoggingExcludedTests
+//
+//  Created by Anh Đức Lê on R 1/07/19.
+//  Copyright © Reiwa 1 Anh Đức Lê. All rights reserved.
+//
+
+import XCTest
+@testable import LoggingExcluded
+
+class LoggingExcludedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
